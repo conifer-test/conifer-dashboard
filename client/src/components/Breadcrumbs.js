55906fe6e1d1ac57d3dc5567303cf914a0d529ea
Lawrence Tam <lawrenceatam@gmail.com>
2022-07-25 14:37:51 -0700

    Fix dashboard links

--- client/src/components/Breadcrumbs.js
+++ client/src/components/Breadcrumbs.js
@@ -1,39 +1,44 @@
-import {
-  Link,
-  useLocation,
-} from "react-router-dom";
-
+import { Link, useLocation } from 'react-router-dom';
 
 const BreadCrumbs = ({ testRunID }) => {
-  const location = useLocation()
+  const location = useLocation();
 
   return (
-    <div className="flex p-3 mb-2 bg-gray-200 rounded">
-      <ol className="flex items-center text-gray-800">
-        <li className="flex items-center">
-          <Link to="/testRuns" className="font-bold">Test Runs</Link>
+    <div className='flex p-3 mb-2 bg-gray-200 rounded'>
+      <ol className='flex items-center text-gray-800'>
+        <li className='flex items-center'>
+          <Link reloadDocument to='/testRuns' className='font-bold'>
+            Test Runs
+          </Link>
         </li>
 
-        { location.pathname === '/testRuns' ? 
-          undefined
-          :
+        {location.pathname === '/testRuns' ? undefined : (
           <>
-            <span className="ml-2">
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none"
-                viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                  d="M9 5l7 7-7 7" />
+            <span className='ml-2'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='w-4 h-4'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M9 5l7 7-7 7'
+                />
               </svg>
             </span>
 
-            <li className="flex items-center ml-2">
-              <Link to={`/testRuns/${testRunID}`} >Test Run - {testRunID}</Link>
+            <li className='flex items-center ml-2'>
+              <Link to={`/testRuns/${testRunID}`}>Test Run - {testRunID}</Link>
             </li>
           </>
-        }
+        )}
       </ol>
     </div>
   );
-}
+};
 
 export default BreadCrumbs;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,14 @@
-import React from "react";
-import SmallCard from "./SmallCard"
+import React from 'react';
+import SmallCard from './SmallCard';
 
 const Home = () => {
   return (
-    <div className="w-full px-4 py-2 bg-gray-200 lg:w-full">
-      <div className="container mx-auto mt-12">
-
+    <div className='w-full px-4 py-2 bg-gray-200 lg:w-full'>
+      <div className='container mx-auto mt-12'>
         <SmallCard total={'Hello World'} description={' Documentation'} />
-
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
