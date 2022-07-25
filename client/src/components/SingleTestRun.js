b4584e9cc630d0f04412b9f9e2af9b7fbd35dd3f
Lawrence Tam <lawrenceatam@gmail.com>
2022-07-24 17:17:47 -0700

    Add dashboard SSE
    Co-authored-by: Lawrence Tam <lawrenceatam@gmail.com>
    Co-authored-by: Ainaa Sakinah <ainaasakinah@gmail.com>

--- client/src/components/SingleTestRun.js
+++ client/src/components/SingleTestRun.js
@@ -1,21 +1,43 @@
-import Table from "./Table";
-import TestRunTile from "./TestRunTile";
-import { useParams } from "react-router-dom";
-import BreadCrumbs from "./Breadcrumbs";
+import Table from './Table';
+import TestRunTile from './TestRunTile';
+import { useParams } from 'react-router-dom';
+import BreadCrumbs from './Breadcrumbs';
 
 const SingleTestRun = () => {
   const { id } = useParams();
 
+  const [testFiles, setTestFiles] = useState([]);
+
+  useEffect(() => {
+    const fetchTestFiles = async () => {
+      const { data } = await axios.get(
+        `http://localhost:5001/api/testRuns/${testRunID}`
+      );
+      setTestFiles(data);
+    };
+
+    fetchTestFiles();
+  }, [testRunID]);
+
+  const sse = new EventSource('http://localhost:5001/sse');
+
+  sse.addEventListener('message', ({ data }) => {
+    const updatedData = JSON.parse(data);
+    console.log(data);
+    // If data is sending the entire test files for a run, use setTestFiles and update entire thing
+    // else, we update only the data that has been updated using map or something like that
+  });
+
   return (
-    <div className="w-full px-4 py-2 bg-gray-200 lg:w-full">
-      <div className="container mx-auto mt-12">
-        <BreadCrumbs testRunID={id}/>
-        
+    <div className='w-full px-4 py-2 bg-gray-200 lg:w-full'>
+      <div className='container mx-auto mt-12'>
+        <BreadCrumbs testRunID={id} />
+
         <TestRunTile testRunID={id} />
-        <Table testRunID={id} />
+        <Table testFiles={testFiles} testRunID={id} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SingleTestRun;
