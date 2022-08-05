6f2e4c4f0e94c95569631a5cd93b429a1e57dbd2
Ahmad Jiha <ahmad.j.jiha@gmail.com>
2022-08-05 16:47:02 -0700

    Begin adding live artifacts

--- server/controllers/testsController.js
+++ server/controllers/testsController.js
@@ -4,20 +4,40 @@ const Test = require('../models/test.js');
 // const { areTasksRunning } = require('../utils/areTasksRunning');
 // require('dotenv').config();
 
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+// ...
+
 const getTestRuns = async (req, res, next) => {
   const testRuns = await Test.getAll();
   return res.json(testRuns);
 };
 
+const mapToSignedUrl = async (singleTestRunData) => {
+  const client = new S3Client({ region: 'us-west-1' });
+  const getObjectParams = {
+    Bucket: 'conifer-test-bucket-b586993c-2641-45fc-a6d0-1edf75b711ca',
+    Key: singleTestRunData.videoUrl
+  }
+
+  const command = new GetObjectCommand(getObjectParams);
+  const signedUrl = await getSignedUrl(client, command, { expiresIn: 3600 });
+  singleTestRunData.signedUrl = signedUrl
+  return singleTestRunData;
+}
+
 const getTestRun = async (req, res, next) => {
   try {
     const testrunID = req.params.id;
     const testRunData = await dynamo.getSingle(testrunID);
+    await Promise.all(testRunData.map(mapToSignedUrl));
+
     res.json(testRunData);
   } catch (e) {
-    return next(new HttpError("TestRunID is missing or invalid", 404));
+    console.log(e)
+    return next(new HttpError('TestRunID is missing or invalid', 404));
   }
-}
+};
 
 // Import the Arns of the initiated tasks
 // const taskArns = process.env.TASK_ARNS;
@@ -26,7 +46,5 @@ const getTestRun = async (req, res, next) => {
 // INSERT RETRIEVE TEST RUN ID
 // INSERT CALL TO POLL DYNODB, USE
 
-
-
 exports.getTestRuns = getTestRuns;
-exports.getTestRun = getTestRun; 
+exports.getTestRun = getTestRun;
