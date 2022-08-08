eb514cf93a7a7c7c9a57f8772617cd75d2ad87e9
Lawrence Tam <lawrenceatam@gmail.com>
2022-08-07 23:05:53 -0700

    remove console log

--- server/utils/getItemsByTestRunID.js
+++ server/utils/getItemsByTestRunID.js
@@ -3,23 +3,24 @@ const { unmarshall } = require('@aws-sdk/util-dynamodb');
 const { ddbDocClient } = require('./ddb-doc-client.js');
 const fs = require('fs');
 
-const { ConiferCdkStack: { tableName } } = JSON.parse(fs.readFileSync('../../cdk_outputs.json'));
+const {
+  ConiferCdkStack: { tableName },
+} = JSON.parse(fs.readFileSync('../../cdk_outputs.json'));
 
 const getItemsByTestRunID = async (testRunID) => {
-  console.log(tableName)
   try {
     const params = {
       TableName: tableName,
       IndexName: 'testRun',
       KeyConditionExpression: 'testRunID = :tri',
       ExpressionAttributeValues: {
-        ':tri': { 'S': testRunID }
-      }
+        ':tri': { S: testRunID },
+      },
     };
 
     const data = await ddbDocClient.send(new QueryCommand(params));
 
-    const results = data.Items.map(item => unmarshall(item));
+    const results = data.Items.map((item) => unmarshall(item));
 
     return results;
   } catch (err) {
